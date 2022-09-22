08369e0acb32b432246d4c8f0797d7fee2d14ef6
Trillzdboy <chukwudubem7@gmail.com>
2022-09-22 20:54:36 +0100

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import { MongoClient, ObjectId } from "mongodb";
+import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = (props) => {
+  return (
+    <>
+    <Head>
+        <title>{props.meetupData.title} | using nextjs</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+    <MeetupDetail
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      adderss={props.meetupData.address}
+      description={props.meetupData.description}
+    />
+    </>
+  );
+};
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://trillzdboy:Janeal0y@cluster0.t4rsnuw.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://trillzdboy:Janeal0y@cluster0.t4rsnuw.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+        address: selectedMeetup.address
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
